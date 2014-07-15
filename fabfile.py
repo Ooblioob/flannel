9afb0551e1120931223b96369afaf4c2c4ea1871
davisd <daniel.davis@excella.com>
2014-07-15 15:56:01 -0400

    Fixing a small typo with git checkout's syntax in the case of a tag

--- fabfile.py
+++ fabfile.py
@@ -197,7 +197,7 @@ def git_clone(type, repo_name, url, user):
 def git_stash_and_fetch(branch):
   sudo('git stash')
   sudo('git fetch origin')
-  sudo('git checkout origin/%s' % (branch))
+  sudo('git checkout origin %s' % (branch))
 
 def install_extension_from_wp(type, name, version):
   if version == 'master':
